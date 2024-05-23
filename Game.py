3f2e57c219b1b260e2592104b369e3d3d788ddcb
Charlie Ni <ziao.charlie.ni@gmail.com>
2024-05-23 11:50:52 -0400

    added basic outline + test code

--- Game.py
+++ Game.py
@@ -1,2 +1,22 @@
 #This is where all the game code will be
 
+#Tanks:
+
+#Physics engine: Gravity and power
+
+#Be able to move tanks left and right
+
+#Menu system -> Adjust power, angle, type of gun etc
+
+#Settings/preferences: Music On/Off
+
+#Get points for killing
+
+#Random terrain generation (For later)
+
+#Shop -> Buy powerups, new weapons, skins (colors, do later)
+
+print ("hello world")
+
+
+
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-README
+# TanksVideoGameICS3uCPT
 ICS3u CPT: Tanks
 
 This is the ICS3u CPT for Charlie, Ryan and Caleb.
 
-# TanksVideoGameICS3uCPT
+
