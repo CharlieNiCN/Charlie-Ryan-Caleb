cf35404bd842e0ac4d02efa9822c34ea6f6228e4
RHuangcode <159318516+RyanHuangcodes@users.noreply.github.com>
2024-05-23 13:27:38 -0400

    Update Game.py
    added rectangle for play button - not finished

--- Game.py
+++ Game.py
@@ -26,9 +26,14 @@ pygame.init()
 WIDTH = 640
 HEIGHT = 480
 SIZE = (WIDTH, HEIGHT)
+white = (255,255,255) 
+lightcolor = (170,170,170)  
+dark = (100,100,100) 
 
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
+width = screen.get_width() 
+height = screen.get_height() 
 
 # ---------------------------
 # Initialize global variables
@@ -45,17 +50,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # GAME STATE UPDATES
-    # All game math and comparisons happen here
-
-    # DRAWING
+    
     screen.fill((255, 255, 255))  # always the first drawing command
-
-    pygame.draw.circle(screen, (0, 0, 255), (circle_x, circle_y), 30)
-
-    # Must be the last two lines
-    # of the game loop
+    pygame.draw.rect(screen,dark,[width - 500,height - 350,240,80]) 
+    mouse = pygame.mouse.get_pos()
+#    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+        
     pygame.display.flip()
+
     clock.tick(30)
     #---------------------------
 
