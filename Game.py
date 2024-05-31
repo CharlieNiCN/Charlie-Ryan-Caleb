0963887730d4e0539aec22fcf1c02f27bf3d88a8
CharlieNiCN <133050322+CharlieNiCN@users.noreply.github.com>
2024-05-31 11:43:20 -0400

    Changed the landspace branch to its actual code
    Code wasn't in landscape branch but was in main on a different file. Made it actually on this file

--- Game.py
+++ Game.py
@@ -1,174 +1,72 @@
-#This is where all the game code will be
-
-#Tanks:
-
-#Physics engine: Gravity and power
-
-#Be able to move tanks left and right
-
-#Menu system -> Adjust power, angle, type of gun etc
-
-#Settings/preferences: Music On/Off
-
-#Get points for killing
-
-#Random terrain generation (For later)
-
-#Shop -> Buy powerups, new weapons, skins (colors, do later)
-
 # pygame template
 
 import pygame
-
+from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
 
 pygame.init()
 
-Width = 640
-Height = 480
-SIZE = (Width, Height)
-white = (255,255,255) 
-lightcolor = (170,170,170)  
-dark = (100,100,100)
-scores = []
+WIDTH = 640
+HEIGHT = 480
+SIZE = (WIDTH, HEIGHT)
 
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
-width = screen.get_width() 
-height = screen.get_height() 
-import math
 
 # ---------------------------
 # Initialize global variables
 
-circle_x = 200
-circle_y = 200
+green_tank_x = 200
+green_tank_y = 200
+green_tank_width = 4
 
-#Bullet trajectory function
-def bullet(inputAngle, inputPower):
-    gravConstant = 20
-    inputAngle = math.radians(inputAngle) #input angle (x) 0<x<90 #these are the variables to control the power, angle of fireing, and the strength of gravity 
-    x = 0.0
-    y = 0.0
-    while y>=0: #displays the equation (use this later on for mapping the trajectory of the bullet)
-        y= (x*math.tan(inputAngle))-(gravConstant*x*x)/(2*inputPower*inputPower*math.cos(inputAngle)*math.cos(inputAngle)) #the physics projective equation (pull this up for the video)
-        x+=1 
-        print("(",x,",", y,")")
+red_tank_x = 600
+red_tank_y = 200
+red_tank_width = 4
 
-def main():
-    running = True
-    play_button = pygame.Rect(Width // 2 - 100, Height // 2 - 100, 200, 50)
-    settings_button = pygame.Rect(Width // 2 - 100, Height // 2 - 30, 200, 50)
-    shop_button = pygame.Rect(Width // 2 - 100, Height // 2 + 40, 200, 50)
-    inventory_button = pygame.Rect(Width // 2 - 100, Height // 2 + 110, 200, 50)
+# make the tank
+green_tank =pygame.image.load("green_tank(1).png")
+green_tank_rect = green_tank.get_rect()
+green_tank.center = screen.get_rect().center
+print(green_tank_rect)
+red_tank =pygame.image.load("red_tank.png")
+red_tank_rect = red_tank.get_rect()
+red_tank.center = screen.get_rect().center
+print(red_tank_rect)
+# ---------------------------
 
-    while running:
+running = True
+while running:
     # EVENT HANDLING
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
     # GAME STATE UPDATES
     # All game math and comparisons happen here
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if play_button.collidepoint(event.pos):
-    #                game_loop()
-                if settings_button.collidepoint(event.pos):
-      #              settings_menu()
-                if shop_button.collidepoint(event.pos):
-      #              shop_menu()
-                if inventory_button.collidepoint(event.pos):
-      #              inventory_menu()
-                keys = pygame.key.get_pressed() 
+    # DRAWING
+    screen.fill((255, 255, 255))  # always the first drawing command
+    pygame.draw.rect(screen, (100, 0, 0), green_tank_rect, green_tank_width)  # draw hit-box
+    pygame.draw.rect(screen, (100, 0, 0), red_tank_rect, red_tank_width)  # draw hit-box
 
-        font = pygame.font.SysFont(None, 55)
-        button_font = pygame.font.SysFont(None, 40)
+    for event in pygame.event.get():
+        if event.type == pygame.K_LEFT and red_tank_x > 0:
+            red_tank_x -= 10
+        if event.type == pygame.K_RIGHT and red_tank_x < 0:
+            red_tank_x += 1
+        if event.type == pygame.K_a and green_tank_x > 0:
+            green_tank_x -= 10
+        if event.type == pygame.K_d and green_tank_x < 0:
+            green_tank_x += 10
+        
 
-        screen.fill((255, 255, 255))  # always the first drawing command
-        draw_Button('Main Menu', font, dark, screen, Width // 2 - 100, Height // 4 - 40)
-        draw_Button('Tanks', font, dark, screen, Width // 2 - 50, Height // 4 - 70)
- # drawing the buttons
-        pygame.draw.rect(screen, lightcolor, play_button)
-        draw_Button('Play', font, dark, screen, play_button.x + 50, play_button.y + 10) 
-        pygame.draw.rect(screen, lightcolor, settings_button)
-        draw_Button('Settings', font, dark, screen, settings_button.x + 25, settings_button.y + 10)        
-        pygame.draw.rect(screen, lightcolor, shop_button)
-        draw_Button('Shop', font, dark, screen, shop_button.x + 50, shop_button.y + 10)      
-        pygame.draw.rect(screen, lightcolor, inventory_button)
-        draw_Button('Inventory', font, dark, screen, inventory_button.x + 25, inventory_button.y + 10)        
-        pygame.display.flip()
-
-
-    pygame.draw.circle(screen, (0, 0, 255), (circle_x, circle_y), 30)
+    
 
     # Must be the last two lines
     # of the game loop
     pygame.display.flip()
     clock.tick(30)
     #---------------------------
-    
-    pygame.quit()
-def draw_Button(text, font, color, surface, x, y):
-        textobj = font.render(text, True, color)
-        textrect = textobj.get_rect()
-        textrect.topleft = (x, y)
-        surface.blit(textobj, textrect)
 
-def inventory_menu():
-        running = True
-        while running:
-            for Py_Event in pygame.event.get():
-                if (Py_Event.type == pygame.QUIT):
-                    running = False
-            screen.fill(white)
-            #inventory menu 
 
-            pygame.display.flip()
-            clock.tick(30)
-        
-def game_loop():
-    running = True
-    while running:
-        for Py_Event in pygame.event.get():
-            if Py_Event.type == pygame.QUIT:
-                running = False
-                main()
-
-        screen.fill(white)
-        # put game code here
-
-        pygame.display.flip()
-        clock.tick(30)   
-
-def settings_menu():
-    running = True
-    while running:
-        for Py_Event in pygame.event.get():
-            if Py_Event.type == pygame.QUIT:
-                running = False
-                main()
-
-        screen.fill(white)
-    #setting
-
-        pygame.display.flip()
-        clock.tick(30)
-    
-
-def shop_menu():
-    running = True
-    while running:
-        for Py_Event in pygame.event.get():
-            if Py_Event.type == pygame.QUIT:
-                running = False
-                main()
-
-        screen.fill(white)
-      #shop
-
-        pygame.display.flip()
-        clock.tick(30)
-    
-
-main()
-
+pygame.quit()
